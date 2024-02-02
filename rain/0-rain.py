0d5965c798bcea429842c6862076ff27b1d1d27c
Adisa-Shobi <s.oadisa.dev@gmail.com>
2024-02-02 09:49:53 +0000

    Update: PEP8 applied

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,20 +1,25 @@
+#!/usr/bin/python3
+
 def rain(walls):
     '''
-    Given a list of non-negative integers representing walls of width 1, calculate how much water will be retained after it rains.
+    Given a list of non-negative integers representing walls of
+    width 1, calculate how much water will be retained after it rains.
     '''
     n = len(walls)
     if n <= 2:
         return 0
     return calculate_units(walls)
 
+
 def calculate_units(walls, idx=1, units=0, pending=[]):
     '''
-    Given a list of non-negative integers representing walls of width 1, calculate how much water will be retained after it rains.
+    Given a list of non-negative integers
+    representing walls of width 1, calculate
+    how much water will be retained after it rains.
     '''
     if idx == len(walls) - 1:
         return units
     depth = [walls[idx-1] - walls[idx], walls[idx+1] - walls[idx]]
-    
     vol = min(depth)
     if vol > 0:
         return calculate_units(walls, idx+1, units+vol, pending=pending)
@@ -22,5 +27,9 @@ def calculate_units(walls, idx=1, units=0, pending=[]):
         pending.append(max(depth))
         return calculate_units(walls, idx+1, units, pending=pending)
     if pending:
-        return calculate_units(walls, idx+1, units+(min(pending) * len(pending)), pending=[])
+        return calculate_units(
+            walls,
+            idx+1, units+(min(pending) * len(pending)),
+            pending=[]
+        )
     return calculate_units(walls, idx+1, units, pending=[])
